6ebb191daa089184cb0e5389f6b2438794ebe256
ehabrino22 <98663001+ehabrino22@users.noreply.github.com>
2023-01-01 14:15:33 +0200

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -55,7 +55,7 @@ async def check_is_joined(message):
         status = await app.get_chat_member("freeeindss", userid)
         return True
     except Exception:
-        await message.reply_text("*انت لست مشترك في قناة البوت freeeindss ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        await message.reply_text("*انت لست مشترك في قناة البوت @freeeindss ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
         return False
 
 # Command
